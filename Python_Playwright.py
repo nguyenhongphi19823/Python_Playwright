6770a7e2b2a7dd5172a5f1ba94d9d6547358b677
nguyenhongphi19823 <nguyenhongphi19823@gmail.com>
2026-02-16 17:12:36 +0700

    ok

--- Python_Playwright.py
+++ Python_Playwright.py
@@ -1,30 +1,54 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
+#
+# def run():
+#     # Khởi tạo Playwright và mở trình duyệt Chromium (giống Chrome)
+#     with sync_playwright() as p:
+#         # headless=False giúp bạn quan sát được trình duyệt chạy thực tế
+#         browser = p.chromium.launch(headless=False)
+#         page = browser.new_page()
+#
+#         # 1. Đi tới trang Google
+#         print("Đang truy cập Google...")
+#         page.goto("https://www.google.com")
+#
+#         # 2. Tìm ô tìm kiếm và nhập nội dung
+#         # Trong Playwright, ta có thể tìm theo thuộc tính 'name' của ô input
+#         page.locator("id=APjFqb").fill("Playwright Python")
+#         page.wait_for_timeout(3000)
+#         # 3. Nhấn phím Enter để tìm kiếm
+#         page.keyboard.press("Enter")
+#
+#         # Đợi một chút để xem kết quả
+#         page.wait_for_timeout(3000)
+#
+#         print("Hoàn thành bài test!")
+#         browser.close()
+#
+#
+# if __name__ == "__main__":
+#     run()
+#
 
-def run():
-    # Khởi tạo Playwright và mở trình duyệt Chromium (giống Chrome)
-    with sync_playwright() as p:
-        # headless=False giúp bạn quan sát được trình duyệt chạy thực tế
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
 
-        # 1. Đi tới trang Google
-        print("Đang truy cập Google...")
-        page.goto("https://www.google.com")
-
-        # 2. Tìm ô tìm kiếm và nhập nội dung
-        # Trong Playwright, ta có thể tìm theo thuộc tính 'name' của ô input
-        page.locator("id=APjFqb").fill("Playwright Python")
-        page.wait_for_timeout(3000)
-        # 3. Nhấn phím Enter để tìm kiếm
-        page.keyboard.press("Enter")
-
-        # Đợi một chút để xem kết quả
-        page.wait_for_timeout(3000)
-
-        print("Hoàn thành bài test!")
-        browser.close()
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-if __name__ == "__main__":
-    run()
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://the-internet.herokuapp.com/login")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("tomsmith")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("SuperSecretPassword!")
+    page.get_by_role("button", name=" Login").click()
 
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
